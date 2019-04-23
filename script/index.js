5d6931f7f03c41daf631a0f3762eaba3a2d64479
Tangerine <lwyyyyyy@yahoo.com>
2019-04-23 20:59:22 +0800

    Just Test

---
+++ script/index.js
@@ -0,0 +1,12 @@
+/* Change the style of menu */
+function menuChange(x){
+	x.classList.toggle("change");
+	$("#conList").toggle(20);
+}
+
+/* Change the style of herf */
+
+/* User Information */
+function userInfo(){
+
+}
