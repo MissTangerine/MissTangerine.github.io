8f141e357cda24da1342aa2a09e5fc928a789246
Tangerine <lwyyyyyy@yahoo.com>
2019-06-13 14:35:03 +0800

    Update: toDoList

--- ToDoList/script/ToDoList.js
+++ ToDoList/script/ToDoList.js
@@ -1,5 +1,5 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
+var myNodelist = document.getElementsByClassName("addList");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("span");
@@ -20,7 +20,7 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
+var list = document.querySelector('#oneList');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'li') {
     ev.target.classList.toggle('checked');
@@ -30,11 +30,12 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
+  li.classList.add("addList");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {} else {
-    document.getElementById("oneList").appendChild(li);
+    document.getElementById("oneList").appendChild(li).classList.add("addList");
   }
   document.getElementById("myInput").value = "";
 
